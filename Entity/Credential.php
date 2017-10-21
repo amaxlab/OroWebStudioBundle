62744e7b44680d2692b14c30cf2c82e3e5709149
Egor Zyuskin <e.zyuskin@tree-soft.com>
2017-10-21 16:12:40 +0600

    Fix extend credential entity

--- Entity/Credential.php
+++ Entity/Credential.php
@@ -11,16 +11,44 @@
 
 namespace AmaxLab\Oro\WebStudioBundle\Entity;
 
+use AmaxLab\Oro\WebStudioBundle\Model\ExtendCredential;
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
+use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\ConfigField;
+use Oro\Bundle\OrganizationBundle\Entity\Organization;
+use Oro\Bundle\UserBundle\Entity\User;
 
 /**
  * @author Egor Zyuskin <ezyuskin@amaxlab.ru>
  *
  * @ORM\Entity(repositoryClass="AmaxLab\Oro\WebStudioBundle\Repository\CredentialRepository")
  * @ORM\Table("web_studio_credential")
+ *
+ * @Config(
+ *      defaultValues={
+ *          "entity"={
+ *              "icon"="fa-key"
+ *          },
+ *          "security"={
+ *              "type"="ACL",
+ *              "group_name"="web-studio",
+ *              "category"="security"
+ *          },
+ *          "merge"={
+ *              "enable"=true
+ *          },
+ *          "ownership"={
+ *              "owner_type"="USER",
+ *              "owner_field_name"="owner",
+ *              "owner_column_name"="user_owner_id",
+ *              "organization_field_name"="organization",
+ *              "organization_column_name"="organization_id"
+ *          }
+ *      }
+ * )
  */
-class Credential
+class Credential extends ExtendCredential
 {
     /**
      * @var int
@@ -65,6 +93,32 @@ class Credential
      * @ORM\Column(name="expired_at", type="date", nullable=true)
      */
     private $expiredAt;
+
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="Oro\Bundle\UserBundle\Entity\User")
+     * @ORM\JoinColumn(name="user_owner_id", referencedColumnName="id", onDelete="SET NULL")
+     * @ConfigField(
+     *      defaultValues={
+     *          "merge"={
+     *              "display"=true
+     *          },
+     *          "dataaudit"={
+     *              "auditable"=true
+     *          }
+     *      }
+     * )
+     */
+    protected $owner;
+
+    /**
+     * @var Organization
+     *
+     * @ORM\ManyToOne(targetEntity="Oro\Bundle\OrganizationBundle\Entity\Organization")
+     * @ORM\JoinColumn(name="organization_id", referencedColumnName="id", onDelete="SET NULL")
+     */
+    protected $organization;
 
     /**
      * @return int
@@ -170,6 +224,46 @@ class Credential
     public function setExpiredAt($expiredAt)
     {
         $this->expiredAt = $expiredAt;
+
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    /**
+     * @param User $owner
+     *
+     * @return $this
+     */
+    public function setOwner($owner)
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * @return Organization
+     */
+    public function getOrganization()
+    {
+        return $this->organization;
+    }
+
+    /**
+     * @param Organization $organization
+     *
+     * @return $this
+     */
+    public function setOrganization($organization)
+    {
+        $this->organization = $organization;
 
         return $this;
     }
